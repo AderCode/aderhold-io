321f208e14956b07b5c28f0725639ac35544c494
Matthew Aderhold <matthewaderhold@gmail.com>
2020-01-29 08:38:01 -0600

    Update index.js
    - configure afterModel to generate and set headTags
    - configure setupController to set the routeTitle to update the title tag in the head of the document

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,17 @@
 import Route from '@ember/routing/route';
+import HeadTagsGenerator from '../constants/meta-tags';
 
 export default Route.extend({
-  
+  afterModel() {
+
+    //* Setup Meta Tags
+    const headTags = HeadTagsGenerator();
+    this.set('headTags', headTags);
+  },
+
+  setupController(controller) {
+    //* Setup Page Title
+    controller.set('routeTitle', 'Home')
+  }
+
 });
